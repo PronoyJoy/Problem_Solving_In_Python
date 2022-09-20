4a7e6d88cd566f49ad5f0e203acb02ed97559fd1
joy <jofficial248@gmail.com>
2022-09-20 16:42:30 +0600

    cdc update

--- asynchronous/coding challenge.py
+++ asynchronous/coding challenge.py
@@ -3,17 +3,16 @@ import asyncio
 async def FirstTask():
     await SecondTask()
     print('FirstTask')
+    await ThirdTask()
 
 async def SecondTask():
     print('SecondTask')
+    
 
 async def ThirdTask():
+    
     print('ThirdTask')
 
-async def main():
 
-    x = asyncio.create_task(FirstTask())
-    y = asyncio.create_task(SecondTask())
-    z = asyncio.create_task(ThirdTask())
 
-asyncio.run(main())
+asyncio.run(FirstTask())
